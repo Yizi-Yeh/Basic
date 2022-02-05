ff69079c0e4e6726dc208c2ea21c3e30d79e2c01
Yizi-Yeh <yizi.yeh@alpha-info.com.tw>
2022-02-05 18:32:48 +0800

    feat: add MathObject

---
+++ MathObject.js
@@ -0,0 +1,20 @@
+
+Math.PI()
+// 3.14159 ...
+
+Math.pow(a,b)
+// return a的b次方
+
+Math.random()
+// return 0~1之間任意數
+
+Math.sqrt()
+// 根號
+
+Math.sqrt()
+// 絕對值
+
+Math.floor()
+// 降成地板
+// 1.18 => 1
+// 100.33 => 100
