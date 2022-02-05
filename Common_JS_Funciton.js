11b5b59d62917dafe9e2b533312c09e197a5eb4c
Yizi-Yeh <yizi.yeh@alpha-info.com.tw>
2022-02-05 13:14:42 +0800

    feat: add Common_JS_Funciton

---
+++ Common_JS_Funciton.js
@@ -0,0 +1,13 @@
+// 常見 JS 函數
+// console.log()
+// alert()
+// prompt()
+
+console.log("learning JS");
+// log => 在控制台內寫內容
+
+window.alert("learning JS")
+// 可在瀏覽器內顯示視窗
+
+window.prompt("請輸入年齡")
+// 在瀏覽器中讓使用者輸入資訊的視窗
